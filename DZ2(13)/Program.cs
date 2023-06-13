b6161ddc631d54964f6d0efeca9e708a15cca096
aimthis <aimthis@mail.ru>
2023-06-14 02:22:08 +0500

    dz2(13) done

--- DZ2(13)/Program.cs
+++ DZ2(13)/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a > 99)
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+    a = a % 10;
+    Console.Write($"Третье число {a}");
+}
+else
+{
+    Console.Write("Нет третьего числа");
+}
