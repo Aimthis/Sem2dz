25c019a1a382c8ef419676c62d0904713c5d4a2c
aimthis <aimthis@mail.ru>
2023-06-17 01:40:08 +0500

    feat: dz21 input request remake

--- DZ21/Program.cs
+++ DZ21/Program.cs
@@ -3,24 +3,24 @@
 // Создаю переменную Ui - User Input на каждую координату
 int UiXa = 0;
 int UiYa = 0;
+int UiZa = 0;
 int UiXb = 0;
 int UiYb = 0;
-int UiXc = 0;
-int UiYc = 0;
+int UiZb = 0;
 // Запросы ввода
 Console.Write("Пожалуйста, введите координату X первого числа: ");
 UiXa = int.Parse(Console.ReadLine());
-Console.Write("Пожалуйста, введите координату X первого числа: ");
+Console.Write("Пожалуйста, введите координату Y первого числа: ");
 UiYa = int.Parse(Console.ReadLine());
-Console.Write("Пожалуйста, введите координату X первого числа: ");
+Console.Write("Пожалуйста, введите координату Z первого числа: ");
+UiZa = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите координату X второго числа: ");
 UiXb = int.Parse(Console.ReadLine());
-Console.Write("Пожалуйста, введите координату X первого числа: ");
+Console.Write("Пожалуйста, введите координату Y второго числа: ");
 UiYb = int.Parse(Console.ReadLine());
-Console.Write("Пожалуйста, введите координату X первого числа: ");
-UiXc = int.Parse(Console.ReadLine());
-Console.Write("Пожалуйста, введите координату X первого числа: ");
-UiYc = int.Parse(Console.ReadLine());
-
+Console.Write("Пожалуйста, введите координату Z второго числа: ");
+UiZb = int.Parse(Console.ReadLine());
+//
 
 
 
