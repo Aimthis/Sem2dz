ece8ff7fc7ff19821cc2e2e26798ea1244593beb
aimthis <aimthis@mail.ru>
2023-06-17 02:17:54 +0500

    feat: dz23 input request

--- DZ23/Program.cs
+++ DZ23/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 23: Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9. 
+5 -> 1, 8, 27, 64, 125
+*/
+// Создаю переменную Ui - User Input
+int Ui = 0;
+// Запрос на ввод "N"
+Console.Write("Пожалуйста, введите пятизначное число: ");
+Ui = int.Parse(Console.ReadLine());
+// Цикл на вывод кубов
+for(int i = 1; i <= Ui; i++)
+{
+    Console.Write("Пожалуйста, введите пятизначное число: ");
+}
