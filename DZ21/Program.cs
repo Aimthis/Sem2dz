17468f1501c95ebe4e7554262e76601ba43970c5
aimthis <aimthis@mail.ru>
2023-06-17 01:34:09 +0500

    feat: dz21 input request

--- DZ21/Program.cs
+++ DZ21/Program.cs
@@ -1,3 +1,27 @@
 ﻿/* Задача 21: Напишите программу, которая принимает 
 на вход координаты двух точек и находит расстояние между ними в 3D пространстве.*/
-Console.WriteLine("Hello, World!");
+// Создаю переменную Ui - User Input на каждую координату
+int UiXa = 0;
+int UiYa = 0;
+int UiXb = 0;
+int UiYb = 0;
+int UiXc = 0;
+int UiYc = 0;
+// Запросы ввода
+Console.Write("Пожалуйста, введите координату X первого числа: ");
+UiXa = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите координату X первого числа: ");
+UiYa = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите координату X первого числа: ");
+UiXb = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите координату X первого числа: ");
+UiYb = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите координату X первого числа: ");
+UiXc = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите координату X первого числа: ");
+UiYc = int.Parse(Console.ReadLine());
+
+
+
+
+    
